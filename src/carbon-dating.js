2d1f68277b7e8f56a371459e07e50acecacf244d
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2021-02-17 23:35:11 +0100

    feat: add solution to carbon dating and extended repeater

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
+
+
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str,opt) {
+  if (typeof str != 'string') {
+    String(str);
+      }
+      let strAdd;
+      let strResult = [];
+      let repeatTimes = opt.repeatTimes;
+      let separator = opt.separator;
+     
+      let additionSeparator = opt.additionSeparator;
+      let additionRepeatTimes = opt.additionRepeatTimes;
+    
+      let addition = String(opt.addition);
+      
+      if(separator === undefined) {
+        separator = '+';
+      }
+      if(repeatTimes === undefined ) {
+        repeatTimes = 1;
+      }
+    
+      if(additionRepeatTimes === undefined) {
+        additionRepeatTimes = 1;
+      } 
+      let additionRepeat = '';
+      for(let i=0; i< additionRepeatTimes; i++) {
+        
+      if(i != 0 && i <= additionRepeatTimes-1) {
+        additionRepeat += additionSeparator+addition;
+      } else {
+        additionRepeat += addition;
+      }
+      }
+      if(additionRepeat === undefined) {
+        addition = '';
+      }
+      if(addition === undefined) {
+        addition = '';
+      }
+      if(additionSeparator === undefined) {
+        additionSeparator = '';
+      }
+     
+    
+    
+      for (let i = 1; i <= repeatTimes; i++) {
+       
+        strAdd = `${str}${additionRepeat}`;
+        strResult.push(strAdd);
+      }
+      return strResult.join(separator);
 };
   
