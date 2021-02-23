94053af3ad9c807fa9c968fc0b95fab1172f9176
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2021-02-23 23:39:57 +0100

    feat: add solution to transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  
+    const discNext = '--discard-next';
+    const discPrev = '--discard-prev';
+    const doubleNext = '--double-next';
+    const doublePrev = '--double-prev';
+    const newArr = [];
+
+    if (typeof arr === 'object') {
+        arr.map((el, index, arr) => {
+
+            if (el == doubleNext) {
+                el = arr[index + 1];
+                return newArr.push(el);
+            } else if (el == doublePrev) {
+                el = arr[index - 1];
+            } else if (el == discNext) {
+                return arr.splice(el, 1);
+            }
+            else if (el == discPrev) {
+                el = arr[index - 1];
+                return newArr.pop();
+            } else {
+                newArr.push(el);
+            } 
+        });
+
+    } else {
+        throw new Error('Error');
+    }
+    return newArr.filter(el=> el!== undefined);
+
+
 };
